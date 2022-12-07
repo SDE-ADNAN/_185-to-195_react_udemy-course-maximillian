1d01200eccd366bbc2a8ca4495f4eda652548a19
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-12-07 08:53:02 +0530

    updated the logic of useCounter hook to make it reusable in both forward and backward counter

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,15 +1,22 @@
 import { useState, useEffect } from "react";
 
-const useCounter = () => {
+const useCounter = (forward = true) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if (forward) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [forward]);
+  // here we are passing the forward variable as a dependency to the useEffect hook
+  // so that the useEffect hook will run again if the forward variable changes in case
+  // we change the direction of the counter
 
   return counter;
 };
